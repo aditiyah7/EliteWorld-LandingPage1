c535cd880c427d1ee0091c66f7310a0686b28d3a
Sibsankar-de <sibsankarde910@gmail.com>
2025-08-13 00:11:38 +0530

    Update commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,12 +29,12 @@ export default function Home() {
     // Change image after 4s
     const timer = setTimeout(() => {
       setImgId(prev => (prev === images.length - 1 ? 0 : prev + 1));
-    }, 10000);
+    }, 5000);
 
     return () => clearTimeout(timer);
   }, [imgId]);
 
-  const [timeLeft, setTimeLeft] = useState(30);
+  const [timeLeft, setTimeLeft] = useState(10);
 
   useEffect(() => {
     if (timeLeft <= 0) return; // Stop when it reaches 0
@@ -57,16 +57,20 @@ export default function Home() {
       <div className="fixed top-0 left-0 w-full h-full -z-1" ><img src={showImg} ref={imgRef} alt="" className="w-full h-full object-cover bg-img" draggable={false} /></div>
 
       <div className="w-screen min-h-screen flex flex-col items-center">
-        <div className="mt-15 mb-4"><img src="/logo-crypto.png" alt="" className="rounded-full w-30 h-30" /></div>
-        <div className="bg-[#2c2c2c5b] backdrop-blur-2xl md:w-[35em] w-[95vw]  p-10  rounded-xl">
-          <h1 className="text-center text-3xl font-bold mb-5">GET EXCLUSIVE ACCESS 🚀</h1>
-          <h4 className="text-center text-xl mb-2">Join the <strong>World's No. 1</strong> <strong className="text-[#e4ad15]">Elites Group ✨</strong></h4>
-          <h5 className="text-center text-lg text-gray-300">Expert Strategies • Precision Signals • Consistent Profits</h5>
-          <div className="text-center text-3xl my-4 float-anim">👇</div>
+        <div className="mt-5 mb-3"><img src="/crypto-logo2.jpg" alt="" className="rounded-full w-30 h-30" /></div>
+        <div className="bg-[#2c2c2c5b] backdrop-blur-2xl md:w-[35em] w-[85vw]  p-5  rounded-xl">
+          <h1 className="text-center text-[1.2em] font-bold mb-3">GET EXCLUSIVE ACCESS 🚀</h1>
+          <h4 className="text-center text-lg mb-1">
+            <div>Join the <strong>World's No. 1</strong> </div>
+            <div><strong className="text-[#e4ad15]">Bug Strategy Community</strong></div>
+            <div className="mt-2"><strong className="text-xl">💰 Elite's World 💰</strong></div>
+          </h4>
+          <h5 className="text-center text-gray-300">Expert Strategies • Precision Signals • Consistent Profits</h5>
+          <div className="text-center text-2xl mb-3 float-anim">👇</div>
           <div>
             <a href="https://t.me/+jl86GPY_X4lhZDU1" target="_blank">
-              <button className="p-5 blue-shade-bg flex items-center justify-center gap-5 text-white w-full rounded-2xl cursor-pointer animated-rect hover:scale-103 transition-all duration-200 active:scale-95 active:brightness-80">
-                <div><img src="telegram-logo.webp" alt="" className="w-10 h-10" /></div>
+              <button className="px-5 py-3 blue-shade-bg flex items-center justify-center gap-5 text-white w-full rounded-2xl cursor-pointer animated-rect hover:scale-103 transition-all duration-200 active:scale-95 active:brightness-80">
+                <div><img src="telegram-logo.webp" alt="" className="w-[60px] h-[60px] flex" /></div>
                 <div>
                   <div>JOIN OUR TELEGRAM COMMUNITY</div>
                 </div>
@@ -74,7 +78,34 @@ export default function Home() {
             </a>
           </div>
         </div>
-        <div className="mt-2 font-bold bg-[#414141a0] backdrop-blur-2xl py-1 px-3 rounded-2xl">⌚ Join now in {timeLeft}s</div>
+        <div className="my-1 flex items-center justify-center gap-2">
+          <span><img src="telegram-logo.webp" alt="telegram" className="w-5 h-5" /></span>
+          <span>Your'e invited to <strong>Elite's World</strong></span>
+        </div>
+        <div>
+          <div className="text-center text-2xl">Hurry Up!</div>
+          <div className="flex gap-2 items-start">
+            <div className="flex flex-col items-center">
+              <span className="font-bold text-4xl">00</span>
+              <span className="text-sm text-gray-300">DAYS</span>
+            </div>
+            <div className="mt-0 text-2xl">:</div>
+            <div className="flex flex-col items-center">
+              <span className="font-bold text-4xl">00</span>
+              <span className="text-sm text-gray-300">HRS</span>
+            </div>
+            <div className="mt-0 text-2xl">:</div>
+            <div className="flex flex-col items-center">
+              <span className="font-bold text-4xl">00</span>
+              <span className="text-sm text-gray-300">MINS</span>
+            </div>
+            <div className="mt-0 text-2xl">:</div>
+            <div className="flex flex-col items-center">
+              <span className="font-bold text-4xl">{String(timeLeft).padStart(2, '0')}</span>
+              <span className="text-sm text-gray-300">SECS</span>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
